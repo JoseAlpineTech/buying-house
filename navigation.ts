43c6e809646b886293efef18bd1f02e128cb279c
Jose Palacios <jose@alpinetech.ca>
2025-11-16 18:08:07 -0700

    Adds more languages / translations

--- navigation.ts
+++ navigation.ts
@@ -1,6 +1,16 @@
 import { createNavigation } from "next-intl/navigation";
 
-export const locales = ["en", "es"] as const;
+export const locales = [
+  "en",
+  "es",
+  "fr",
+  "de",
+  "it",
+  "ja",
+  "pt",
+  "ko"
+] as const;
+
 export type Locale = (typeof locales)[number];
 
 export const defaultLocale: Locale = "en";
